b0fd6e2a674f57ea47108bb33a4d62880cc10bb3
Michael Christenson <mchristenson5891@gmail.com>
2020-01-10 15:34:56 -0800

    fix starter code bug

--- units/2_full_stack_dev/week_1/day_5/1_mongoose_referencing /starter-code/mongoose-movies/controllers/movies.js
+++ units/2_full_stack_dev/week_1/day_5/1_mongoose_referencing /starter-code/mongoose-movies/controllers/movies.js
@@ -1,20 +1,20 @@
 const Movie = require('../models/movie');
 
-const index = (req, res) {
+const index = (req, res) => {
   Movie.find({}, (err, movies) => {
     res.render('movies/index', { title: 'All Movies', movies });
   });
-}
+};
 
 const show = (req, res) => {
   Movie.findById(req.params.id, (err, movie) => {
     res.render('movies/show', { title: 'Movie Detail', movie });
   });
-}
+};
 
-const newMovie = (req, res) =>{
+const newMovie = (req, res) => {
   res.render('movies/new', { title: 'Add Movie' });
-}
+};
 
 const create = (req, res) => {
   // convert nowShowing's checkbox of nothing or "on" to boolean
@@ -23,16 +23,16 @@ const create = (req, res) => {
     if (req.body[key] === '') delete req.body[key];
   }
   var movie = new Movie(req.body);
-  movie.save((err) => {
+  movie.save(err => {
     if (err) return res.redirect('/movies/new');
     console.log(movie);
     res.redirect('/movies');
   });
-}
+};
 
 module.exports = {
   index,
   show,
   new: newMovie,
-  create
+  create,
 };
