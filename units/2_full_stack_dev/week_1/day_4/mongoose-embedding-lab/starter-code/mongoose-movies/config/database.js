29f297d4d119f5773f9498a32d43da2d381799db
mchristenson5891 <michael.christenson@generalassemb.ly>
2020-01-10 08:45:09 -0800

    Update database.js

--- units/2_full_stack_dev/week_1/day_4/mongoose-embedding-lab/starter-code/mongoose-movies/config/database.js
+++ units/2_full_stack_dev/week_1/day_4/mongoose-embedding-lab/starter-code/mongoose-movies/config/database.js
@@ -1,11 +1,11 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/movies',
   { useNewUrlParser: true, useCreateIndex: true }
 );
 
 // shortcut to mongoose.connection object
-var db = mongoose.connection;
+const db = mongoose.connection;
 
 db.on('connected', function () {
   console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
