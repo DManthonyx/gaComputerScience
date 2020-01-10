1178506592ccf6b42cc4c2581fcfa9dc11d83e8f
mchristenson5891 <michael.christenson@generalassemb.ly>
2020-01-10 08:44:40 -0800

    Update server.js

--- units/2_full_stack_dev/week_1/day_4/mongoose-embedding-lab/starter-code/mongoose-movies/server.js
+++ units/2_full_stack_dev/week_1/day_4/mongoose-embedding-lab/starter-code/mongoose-movies/server.js
@@ -1,14 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 require('./config/database');
 
-var indexRouter = require('./routes/index');
-var moviesRouter = require('./routes/movies');
+const indexRouter = require('./routes/index');
+const moviesRouter = require('./routes/movies');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
