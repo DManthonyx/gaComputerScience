11d331269cfb195d4d7230f8c879812f32dba1e6
Michael Christenson <mchristenson5891@gmail.com>
2020-01-10 15:33:24 -0800

    update started code

--- units/2_full_stack_dev/week_1/day_5/1_mongoose_referencing /starter-code/mongoose-movies/controllers/reviews.js
+++ units/2_full_stack_dev/week_1/day_5/1_mongoose_referencing /starter-code/mongoose-movies/controllers/reviews.js
@@ -1,14 +1,14 @@
 var Movie = require('../models/movie');
 
-module.exports = {
-  create
-};
-
-function create(req, res) {
+const create = (req, res) => {
   Movie.findById(req.params.id, function(err, movie) {
     movie.reviews.push(req.body);
-    movie.save(function(err) {
+    movie.save(err => {
       res.redirect(`/movies/${movie._id}`);
     });
   });
-}
+};
+
+module.exports = {
+  create,
+};
